d90080e8d5407ea0e1ec6489b6eaa6056e75f7ba
Omar Anaya <omaranaya616@gmail.com>
2024-01-30 23:20:23 -0600

    Cambios a problemas de freecodecamp

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,41 +1,36 @@
+
 function telephoneCheck(str) {
 
-   // let onlyNumbers = str.replace(/[)(-\s^a-z]/gi, '');
-   let onlyNumbers = str.replace(/[^0-9]/gi, '');
-console.log(str, '|', onlyNumbers, '|', onlyNumbers.length);
-
-   if(onlyNumbers.length > 9 && onlyNumbers.length < 12){
-
-      if(onlyNumbers.length == 11 && onlyNumbers.at(0) != 1)
-         return false;
-
-      // let regexOnlyUSA = /[0-9)(-\s]/g;
-      let regexOnlyUSA = /[0-9)(-\s]/g;
-      // console.log(str.match(regexOnlyUSA));  
-      console.log(regexOnlyUSA.test(str));  
-   }
-
-   return '0';
+   let regexTelUSA = /1[\s0-9]/g;
+console.log(regexTelUSA.test(str), str.match(regexTelUSA));
+   return regexTelUSA.test(str);
  }
  
  let response;
  
- response = telephoneCheck("555-555-5555");
- console.log(response, '\n');
+//  response = telephoneCheck("555-555-5555");
+//  console.log(response, '\n');
  
- response = telephoneCheck("555 555 5555");
- console.log(response, '\n');
+//  response = telephoneCheck("555 555 5555");
+//  console.log(response, '\n');
 
- response = telephoneCheck("(555) 555 5555");
- console.log(response, '\n');
+//  response = telephoneCheck("(555) 555 5555");
+//  console.log(response, '\n');
 
  response = telephoneCheck("1 (555) 555 5555");
+ console.log(response, '\n');
+
+ response = telephoneCheck("2 (555) 555 5555");
  console.log(response, '\n');
 
  response = telephoneCheck("1 (555 555 5555");
  console.log(response, '\n');
 
- response = telephoneCheck("123**&!!asdf#");
+//  response = telephoneCheck("123**&!!asdf#");
+//  console.log(response, '\n');
+
+ response = telephoneCheck("-1 (757) 622-7382")
  console.log(response, '\n');
- response = telephoneCheck("123**&!!#[");
- console.log(response, '\n');
+
+//  response = telephoneCheck("(555)5(55?)-5555");
+//  console.log(response, '\n');

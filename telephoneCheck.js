3a6213281c0aee74dfb5828ab2a5279cf6a6f38b
Omar Anaya <omaranaya616@gmail.com>
2024-02-01 18:35:51 -0600

    Peuqeño cambio a problema de freecodecamp

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,15 +1,24 @@
 
 function telephoneCheck(str) {
 
-   let regexTelUSA = /1[\s0-9]/g;
+   // str = str.replace(/[^0-9)(]/g, '')
+console.log(str);
+   // let regexTelUSA = /1[\s-]?[(]?[0-9]+[)]?[\s-]?[0-9]+[\s-]?[0-9]+/g;
+   let regexTelUSA = /1[\s-]?[(0-9)]+[\s-]?[0-9]+[\s-]?[0-9]+/g;
 console.log(regexTelUSA.test(str), str.match(regexTelUSA));
    return regexTelUSA.test(str);
  }
  
  let response;
  
-//  response = telephoneCheck("555-555-5555");
-//  console.log(response, '\n');
+ response = telephoneCheck("555-555-5555");
+ console.log(response, '\n');
+ 
+ response = telephoneCheck("1 555-555-5555");
+ console.log(response, '\n');
+
+ response = telephoneCheck("1555-555-5555");
+ console.log(response, '\n');
  
 //  response = telephoneCheck("555 555 5555");
 //  console.log(response, '\n');

74f19ecd0dcb7c296bb0bdca7b6af0ffa2ab0ca9
Omar Anaya <omaranaya616@gmail.com>
2024-03-19 18:42:36 -0600

    Mis cambios a problema de freecodecamp

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,6 +1,10 @@
 
 function telephoneCheck(str) {
 
+   let regexFistValidate = /^[0-9)(-]/g;
+   let firstValidate = regexFistValidate.test(str);
+   console.log("fist Validate", firstValidate);
+
    let onlyNumbers = str.replace(/[^0-9]/g, '');
    console.log(str, onlyNumbers, onlyNumbers.length);
    
@@ -8,16 +12,19 @@ function telephoneCheck(str) {
    let regexTelUSA;
    if(onlyNumbers.length == 11){
       
-      regexTelUSA = /^1?[\s-][(0-9)]+[\s-]?[0-9]+[\s-]?[0-9]+/g;
-      console.log(regexTelUSA.test(str), str.match(regexTelUSA));
+      // regexTelUSA = /^1?[\s-][(0-9)]+[\s-]?[0-9]+[\s-]?[0-9]+/g;
+      regexTelUSA = /^1?[(\s-][(0-5)]+[\s-]?[0-9]+[\s-]?[0-9]+|^5+[0-9]/g;
+      console.log("IF", regexTelUSA.test(str), str.match(regexTelUSA));
       response = regexTelUSA.test(str);
 
    } else if(onlyNumbers.length == 10){
       
-      regexTelUSA = /[^( 0-9]+[\s-]?[0-9]+[\s-]?[0-9]+/g;
-      console.log(regexTelUSA.test(str), str.match(regexTelUSA));
+      // regexTelUSA = /[^( 0-9]+[\s-]?[0-9]+[\s-]?[0-9]+/g;
+      regexTelUSA = /[(]?[^( 0-5]+[\s-]?[0-9]+[\s-]?[0-9]+|^5+[0-9]/g;
+      console.log("ELSE IF", regexTelUSA.test(str), str.match(regexTelUSA));
       response = regexTelUSA.test(str);
-
+   } else {
+      response = false;
    }
 
    if(str.includes('(') && !str.includes(')') || !str.includes('(') && str.includes(')'))
@@ -28,14 +35,11 @@ function telephoneCheck(str) {
  
  let response;
  
- response = telephoneCheck("1 (555)555-5555");
- console.log(response, '\n');
-
- response = telephoneCheck("1 555)555-5555");
+ response = telephoneCheck("(555)555-5555") // true 1
  console.log(response, '\n');
  
- response = telephoneCheck("(6054756961)");
+ response = telephoneCheck("1(555)555-5555") // true 2
  console.log(response, '\n');
-
- response = telephoneCheck("55 5555 5555");
+ 
+ response = telephoneCheck("55 55-55-555-5") // false 3
  console.log(response, '\n');

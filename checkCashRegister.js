236de683287b749568172ed1a75b653f6cf44a3a
Omar Anaya <omaranaya616@gmail.com>
2024-05-12 23:00:11 -0600

    Cambios para tratar de tomar en cuenta el cambio que se pasa en el arreglo a la funcion

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -1,10 +1,12 @@
 function checkCashRegister(price, cash, cid) {
 
-  let totalCash_CID = (cid.reduce( (accum, elem) => accum + (elem[1]),  0)).toFixed(2);
-  totalCash_CID = Number(totalCash_CID);
+  // let totalCash_CID = (cid.reduce( (accum, elem) => accum + (elem[1]),  0)).toFixed(2);
+  // totalCash_CID = Number(totalCash_CID);
+
+  cid = cid.reverse();
 
   let returnCash = cash - price;
-
+console.log(returnCash);
   let valueMoney = [
     {name: 'ONE HUNDRED', value: 100}, 
     {name: 'TWENTY', value: 20}, 
@@ -16,7 +18,8 @@ function checkCashRegister(price, cash, cid) {
     {name: 'NICKEL', value: .05}, 
     {name: 'PENNY', value: .01}, 
   ];
-  // let valueMoney = [.01, .05, .1, .25, 1, 5, 10, 20, 100];
+
+  // valueMoney = valueMoney.reverse();
 
   let cidReturn = { STATUS: "", CHANGE: [] };
   let valueChange = {};
@@ -26,23 +29,31 @@ function checkCashRegister(price, cash, cid) {
 
     if(returnCash >= valueMoney[i].value){
 
-      // Hay que revisar para hacer un nuevo objecto y agregar al ararya para desupes tratar de sumar los cambios
-      if(valueChange[valueMoney[i].name] == undefined)
-        valueChange[valueMoney[i].name] = valueMoney[i].value;
-      else
-        valueChange[valueMoney[i].name] += valueMoney[i].value;
+      let compareTotalSumReturn = valueChange[valueMoney[i].value] == undefined ? 0 : valueMoney[i].value;
 
-// console.log(valueChange);
+      console.log(compareTotalSumReturn, valueChange[valueMoney[i].value], i);
 
-      // cidReturn.CHANGE.push( valueChange );
+      if(compareTotalSumReturn < cid[i][1]){
+
+        if(valueChange[valueMoney[i].name] == undefined)
+          valueChange[valueMoney[i].name] = valueMoney[i].value;
+        else
+          valueChange[valueMoney[i].name] += valueMoney[i].value;
+
+        // cidReturn.CHANGE.push( valueChange );
+
+        // console.log(valueChange);
+      }
 
       returnCash -= valueMoney[i].value;
-      i=0;
-    }
+
+      console.log(returnCash);
+    } 
 
     if(returnCash <= 0)
       isAllChange = false;
 
+    if(i < (valueMoney.length-1))
       i++;
   }
 
@@ -54,6 +65,6 @@ function checkCashRegister(price, cash, cid) {
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
-checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])

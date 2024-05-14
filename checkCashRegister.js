5022161b2c43567ddba3bdc87f4bd60d248ee048
Omar Anaya <oanaya@sinci.com>
2024-05-13 18:40:50 -0600

    Cambios a proyecto final de curso en freecodecamp#

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -1,63 +1,51 @@
 function checkCashRegister(price, cash, cid) {
 
-  // let totalCash_CID = (cid.reduce( (accum, elem) => accum + (elem[1]),  0)).toFixed(2);
-  // totalCash_CID = Number(totalCash_CID);
-
   cid = cid.reverse();
 
-  let returnCash = cash - price;
-console.log(returnCash);
+  let returnCash = (cash - price).toFixed(2);
+  console.log(returnCash);
   let valueMoney = [
-    {name: 'ONE HUNDRED', value: 100}, 
-    {name: 'TWENTY', value: 20}, 
-    {name: 'TEN', value: 10}, 
-    {name: 'FIVE', value: 5}, 
-    {name: 'ONE', value: 1}, 
-    {name: 'QUARTER', value: .25}, 
-    {name: 'DIME', value: .1}, 
-    {name: 'NICKEL', value: .05}, 
-    {name: 'PENNY', value: .01}, 
+    { name: 'ONE HUNDRED', value: 100 },
+    { name: 'TWENTY', value: 20 },
+    { name: 'TEN', value: 10 },
+    { name: 'FIVE', value: 5 },
+    { name: 'ONE', value: 1 },
+    { name: 'QUARTER', value: .25 },
+    { name: 'DIME', value: .1 },
+    { name: 'NICKEL', value: .05 },
+    { name: 'PENNY', value: .01 },
   ];
 
-  // valueMoney = valueMoney.reverse();
-
-  let cidReturn = { STATUS: "", CHANGE: [] };
   let valueChange = {};
   let isAllChange = true;
   let i = 0;
-  while(isAllChange){
+  while (isAllChange) {
 
-    if(returnCash >= valueMoney[i].value){
+    let compareTotalSumReturn = valueChange[valueMoney[i].name] == undefined ? 0 : valueChange[valueMoney[i].name];
 
-      let compareTotalSumReturn = valueChange[valueMoney[i].value] == undefined ? 0 : valueMoney[i].value;
+    if (returnCash >= valueMoney[i].value && compareTotalSumReturn < cid[i][1]) {
 
-      console.log(compareTotalSumReturn, valueChange[valueMoney[i].value], i);
-
-      if(compareTotalSumReturn < cid[i][1]){
-
-        if(valueChange[valueMoney[i].name] == undefined)
-          valueChange[valueMoney[i].name] = valueMoney[i].value;
-        else
-          valueChange[valueMoney[i].name] += valueMoney[i].value;
-
-        // cidReturn.CHANGE.push( valueChange );
-
-        // console.log(valueChange);
+      if (valueChange[valueMoney[i].name] == undefined) {
+        valueChange[valueMoney[i].name] = valueMoney[i].value;
+      } else {
+        valueChange[valueMoney[i].name] = Number((valueChange[valueMoney[i].name] + valueMoney[i].value).toFixed(2));
       }
 
-      returnCash -= valueMoney[i].value;
+      returnCash = Number((returnCash - valueMoney[i].value).toFixed(2));
+      i = 0;
+      continue;
+    }
 
-      console.log(returnCash);
-    } 
-
-    if(returnCash <= 0)
+    if (returnCash <= 0 || i == (valueMoney.length - 1)) {
       isAllChange = false;
-
-    if(i < (valueMoney.length-1))
+    } else {
       i++;
+    }
   }
 
   console.log(valueChange);
+
+  let cidReturn = { STATUS: "", CHANGE: [] };
 
   let change;
   return change;
@@ -65,6 +53,8 @@ console.log(returnCash);
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
-// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 
-// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])

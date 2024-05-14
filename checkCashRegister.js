869cd64b61354234eca4f54d8971be54bd096a27
Omar Anaya <omaranaya616@gmail.com>
2024-05-14 12:24:44 -0600

    Cambios a proyecto final para certificación en javascript de plataforma freecodecamp

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -1,8 +1,7 @@
 function checkCashRegister(price, cash, cid) {
 
-  cid = cid.reverse();
-
-  let returnCash = (cash - price).toFixed(2);
+  let cidReverse = cid.toReversed();
+  let returnCash = Number((cash - price).toFixed(2));
 
   let valueMoney = [
     { name: 'ONE HUNDRED', value: 100 },
@@ -23,7 +22,7 @@ function checkCashRegister(price, cash, cid) {
 
     let compareTotalSumReturn = valueChange[valueMoney[i].name] == undefined ? 0 : valueChange[valueMoney[i].name];
 
-    if (returnCash >= valueMoney[i].value && compareTotalSumReturn < cid[i][1]) {
+    if (returnCash >= valueMoney[i].value && compareTotalSumReturn < cidReverse[i][1]) {
 
       if (valueChange[valueMoney[i].name] == undefined) {
         valueChange[valueMoney[i].name] = valueMoney[i].value;
@@ -45,26 +44,21 @@ function checkCashRegister(price, cash, cid) {
 
   valueChange = Object.keys(valueChange).map( elem => [elem, valueChange[elem]] );
 
-  let statusChange = (change) => {
+  let cidReturn;
+  let totalCash_CID = (cid.reduce( (accum, elem) => accum + (elem[1]),  0)).toFixed(2);
+  totalCash_CID = Number(totalCash_CID);
+  let totalChange = Number( (valueChange.reduce( (accum, elem) => accum + elem[1], 0 )).toFixed(2) );
+  let cashToReturn = Number((cash - price).toFixed(2));
 
-    let totalCash_CID = (cid.reduce( (accum, elem) => accum + (elem[1]),  0)).toFixed(2);
-    totalCash_CID = Number(totalCash_CID);
-
-    console.log(totalCash_CID);
-
-    return "OPEN";
+  if( totalCash_CID > totalChange && totalChange == cashToReturn ){
+    cidReturn = { status: "OPEN", change: valueChange };
+  } else if(totalCash_CID == totalChange && totalChange == cashToReturn){
+    cidReturn = { status: "CLOSED", change: cid };
+  } else {
+    cidReturn = { status: "INSUFFICIENT_FUNDS", change: [] };
   }
-
-  let cidReturn = { STATUS: statusChange(), CHANGE: valueChange };
-  console.log(cidReturn);
-
-  return valueChange;
+console.log(cidReturn);
+  return cidReturn;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-
 checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
-
-checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
